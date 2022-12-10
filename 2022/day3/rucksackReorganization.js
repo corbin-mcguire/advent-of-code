5bb584e0a4f13be38afdae32cf5322707b4e787c
Corbin McGuire <mcguire.corbin@gmail.com>
2022-12-10 11:34:45 -0500

    part 1 done, working on part 2

---
+++ 2022/day3/rucksackReorganization.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+
+fs.readFile(`${__dirname}/test.txt`, "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+  const rucksacks = data.split(/\n/);
+  const groups = [];
+  let alphabet = [];
+  let prioritySum = 0;
+
+  // get a list of all upper and lower case letters
+  for (let i = 65; i <= 122; i += 1) {
+    // skip to lower case ascii values
+    if (i === 91) {
+      i += 5;
+      continue;
+    }
+    alphabet.push(String.fromCharCode(i));
+  }
+
+  // split the alphabet array and place lower case letters first
+  alphabet = [...alphabet.splice(alphabet.length / 2), ...alphabet];
+
+  // split the elves into groups of 3
+  rucksacks.forEach((rucksack, index) => {
+    if ((index + 1) % 3 === 0) {
+      groups.push([rucksacks[index - 2], rucksacks[index - 1], rucksack]);
+    }
+  });
+
+  groups.forEach((group) => {
+    group.forEach((rucksacks) => {
+      console.log(rucksacks, "\n");
+      // rucksacks.forEach((rucksack) => {
+      //   if (rucksack.length > 0) {
+      //     const compartments = rucksack.split("");
+      //     const compartmentA = compartments
+      //       .splice(0, compartments.length / 2)
+      //       .sort()
+      //       .reverse();
+      //     const compartmentB = [...compartments].sort().reverse();
+      //     let priority = 0;
+      //     compartmentA.forEach((itemA) => {
+      //       compartmentB.find((itemB) => {
+      //         if (itemB === itemA) {
+      //           priority = alphabet.indexOf(itemA) + 1;
+      //         }
+      //       });
+      //     });
+      //     prioritySum += priority;
+      //   }
+      // });
+    });
+  });
+
+  console.log(prioritySum);
+});
